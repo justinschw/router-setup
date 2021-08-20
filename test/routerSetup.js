18b795fec787daedce88b8053ceb12eec574101d
Justin Schwartzbeck <justinmschw@gmail.com>
2021-08-20 16:57:56 -0500

    Adds DHCP test

--- test/routerSetup.js
+++ test/routerSetup.js
@@ -2,6 +2,10 @@
 const Router = require('../lib/routerSetup');
 const expect = require('chai').expect;
 
+function clone(obj) {
+    return JSON.parse(JSON.stringify(obj));
+}
+
 const defaultConfig = {
     wan: {
         iface: 'eth0',
@@ -23,6 +27,22 @@ describe('/lib/routerSetup', function() {
             const router = new Router(defaultConfig);
             expect(Object.keys(router.net.plan.network.ethernets).length).eql(2);
             expect(router.servers.length).eql(0);
+            expect(router.net.plan.network.ethernets.eth1.addresses[0]).eql('192.168.4.1/24')
+            done();
+        });
+
+        it('dhcp', function(done) {
+            let dhcp = clone(defaultConfig);
+            dhcp.lans[0].dhcpServer = {
+                domain: 'example.com'
+            };
+            const router = new Router(dhcp);
+            expect(Object.keys(router.net.plan.network.ethernets).length).eql(2);
+            expect(router.net.plan.network.ethernets.eth1.addresses[0]).eql('192.168.4.1/24')
+            expect(router.servers.length).eql(1);
+            expect(router.servers[0].options.beginIP).eql('192.168.4.2');
+            expect(router.servers[0].options.endIP).eql('192.168.4.254');
+            expect(router.servers[0].options.listenAddress).eql('192.168.4.1');
             done();
         });
     });
